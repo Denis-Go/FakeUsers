b925fd2c24f63a7e2d0288853ecd4248a956bf88
Denis Goshovatyuk <denis1994@live.ru>
2021-06-06 18:12:30 +0200

    descr update

--- FakeUsers/FakeUsers.py
+++ FakeUsers/FakeUsers.py
@@ -1,6 +1,6 @@
-# Scam a scammer 
+# Scam the scammer by feeding fake user data to their server
 # Based on code by Engineer Man https://github.com/engineer-man/youtube https://www.youtube.com/channel/UCrUL8K81R4VBzm-KOYwrcxQ
-# list of popular domains by https://github.com/mailcheck/mailcheck/wiki
+# List of popular domains by https://github.com/mailcheck/mailcheck/wiki
 # Modification: random domains from list are chosen for users
 
 import requests
